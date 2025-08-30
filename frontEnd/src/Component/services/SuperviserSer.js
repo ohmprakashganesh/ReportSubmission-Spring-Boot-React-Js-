573d6a72138d00e013f0b39ce6a0772346934402
ohmprakashganesh <131688194+ohmprakashganesh@users.noreply.github.com>
2025-08-31 02:08:40 +0545

    changed in some backend logi

--- frontEnd/src/Component/services/SuperviserSer.js
+++ frontEnd/src/Component/services/SuperviserSer.js
@@ -4,3 +4,10 @@ export const getAllGroups=async ()=>{
     const obj= await httpClient.get("/api/groups/all");
     return obj.data;
 }
+ 
+export const getSuperviosrByGroupId= async (id)=>{
+    const obj= await httpClient.get(`/api/groups/group/${id}`);
+    return obj.data;
+
+}
+
